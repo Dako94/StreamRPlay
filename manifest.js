a6d34aa921c900933cc51a0066378540820e2738
Dako94 <128719403+Dako94@users.noreply.github.com>
2025-08-31 02:27:00 +0200

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Accedi a tutti i contenuti RaiPlay direttamente su Stremio con ricerca libera",
   logo: "https://www.rai.it/dl/images/2021/12/17/1639751569406_rai-play.png",
   background: "https://www.raiplay.it/cropgd/1920x1080/dl/img-logos/LOGORAI_sfondoNERO.jpg",
-  resources: ["catalog", "stream"],
+  resources: ["catalog", "stream", "meta"],
   types: ["movie", "series"],
   catalogs: [
     {
