2c7838f4b578436b6eb15e9df1cc20f961890b7f
Dako94 <128719403+Dako94@users.noreply.github.com>
2025-08-31 02:24:48 +0200

    Update server-final.js

--- server-final.js
+++ server-final.js
@@ -8,21 +8,35 @@ const manifest = {
     name: 'RaiPlay Italiano',
     description: 'Accedi a tutti i contenuti RaiPlay direttamente su Stremio',
     logo: 'https://www.rai.it/dl/images/2021/12/17/1639751569406_rai-play.png',
-    resources: ['catalog', 'stream', 'meta'],
-    types: ['movie', 'tv'],
+    resources: ['catalog', 'stream'],  // non serve 'meta' qui
+    types: ['movie', 'series'],
     catalogs: [
-        { type: 'tv', id: 'raiplay_series', name: 'Serie TV RaiPlay' },
+        { type: 'series', id: 'raiplay_series', name: 'Serie TV RaiPlay' },
         { type: 'movie', id: 'raiplay_movies', name: 'Film RaiPlay' }
     ]
 };
 
 const builder = new addonBuilder(manifest);
 
+// Catalogo dinamico
 builder.defineCatalogHandler(catalogHandler);
+
+// Stream dinamico
 builder.defineStreamHandler(streamHandler);
+
+// Meta handler minimo per evitare errori Stremio
+builder.defineMetaHandler(async (args) => {
+    return {
+        meta: {
+            id: args.id,
+            name: args.id
+        }
+    };
+});
 
 const PORT = process.env.PORT || 3000;
 
+// ServeHTTP se disponibile
 if (serveHTTP) {
     serveHTTP(builder.getInterface(), { port: PORT });
 } else {
