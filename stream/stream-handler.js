ba836ec256dbc5950c18da65d76ff9ffb0e862ed
Dako94 <128719403+Dako94@users.noreply.github.com>
2025-08-31 02:56:52 +0200

    Update stream-handler.js

--- stream/stream-handler.js
+++ stream/stream-handler.js
@@ -2,6 +2,8 @@ const videoExtractor = require('./video-extractor');
 
 module.exports = async function streamHandler(args) {
     try {
+        // args.id potrebbe essere tipo "movie:1234" da TMDb
+        // qui puoi mappare gli ID TMDb a RaiPlay reali se vuoi
         const streams = await videoExtractor(args.id);
         return { streams };
     } catch (err) {
